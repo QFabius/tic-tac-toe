aa54c461b2d02fb5ecd3f09bbb79f2b34644f20b
quint de kleijn <quintdekleijn@hotmail.com>
2024-11-16 19:42:32 +0100

    animate game

--- tictactoe.py
+++ tictactoe.py
@@ -3,16 +3,21 @@ import numpy as np
 import pygame
 import math
 
-# Variables
+# Parameters
 ROWS = 3
 COLS = 3
 
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+BLUE = (0, 0, 255)
 
 WIDTH = 600
 HEIGHT = 600
 SIZE = (WIDTH, HEIGHT)
+
+CIRCLE = pygame.image.load('img/circle.png')
+CROSS = pygame.image.load('img/cross.png')
 
 # Functions
 def mark(row, col, player):
@@ -28,6 +33,16 @@ def is_board_full():
                 return False
     return True
 
+def draw_board():
+    for col in range(COLS):
+        for row in range(ROWS):
+            if board[row][col] == 1:
+                window.blit(CIRCLE, ((col*200)+50, (row*200)+50))
+            elif board[row][col] == 2:
+                window.blit(CROSS, ((col*200)+50, (row*200)+50))
+    pygame.display.update()
+            
+
 def draw_lines():
     pygame.draw.line(window, BLACK, (200,0), (200,600), 10)
     pygame.draw.line(window, BLACK, (400,0), (400,600), 10)
@@ -36,23 +51,23 @@ def draw_lines():
 
 def is_winning_move(player):
     if player == 1:
-        announcement = "Player 1 Won"
+        winning_color = BLUE
     else:
-        announcement = "Player 2 Won"
+        winning_color = RED
     
     for row in range(ROWS):
         if board[row][0] == player and board[row][1] == player and board[row][2] == player:
-            print(announcement)
+            pygame.draw.line(window, winning_color, (10, (row*200)+100), (WIDTH-10, (row*200)+100), 10)
             return True
     for col in range(COLS):
         if board[0][col] == player and board[1][col] == player and board[2][col] == player:
-            print(announcement)
+            pygame.draw.line(window, winning_color, ((col*200)+100, 10), ((col*200)+100, HEIGHT-10), 10)
             return True
     if board[0][0] == player and board[1][1] == player and board[2][2] == player:
-        print(announcement)
+        pygame.draw.line(window, winning_color, (10, 10), (590, 590), 10)
         return True
     if board[0][2] == player and board[1][1] == player and board[2][0] == player:
-        print(announcement)
+        pygame.draw.line(window, winning_color, (590, 590), (10, 10), 10)
         return True
 
 # Initiate board
@@ -100,5 +115,15 @@ while not game_over:
 
             Turn += 1
             print(board)
+            draw_board()
+    if is_board_full():
+        game_over = True
     if game_over == True:
         print("Game Over")
+        pygame.time.wait(2000)
+        board.fill(0)
+        window.fill(WHITE)
+        draw_lines()
+        draw_board()
+        game_over = False
+        pygame.display.update()
